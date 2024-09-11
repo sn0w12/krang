45a5bd5110eaf3e91900ec5c4e1d80802d8779b2
sn0w12 <lucasskold49@gmail.com>
2024-09-11 12:44:59 +0200

    Initial commit

---
+++ src/test/java/com/example/krang/KrangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.krang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
