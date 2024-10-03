bf091e47b805c1dce1e92324f65c33c4eaefe2a8
Lucas  Sköld <lucasskold49@gmail.com>
2024-10-03 16:40:25 +0200

    Remove old sql

--- media_artist.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE media_artist (
-    media_id BIGINT UNSIGNED,  
-    artist_id BIGINT UNSIGNED,  
-    PRIMARY KEY (media_id, artist_id),
-    FOREIGN KEY (media_id) REFERENCES media(id),
-    FOREIGN KEY (artist_id) REFERENCES artist(id)
-);
--- media_genre.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS media_genre (
-    media_id BIGINT UNSIGNED NOT NULL,
-    genre_id BIGINT UNSIGNED NOT NULL,  -- Ändra till BIGINT UNSIGNED för att matcha genre.id
-    PRIMARY KEY (media_id, genre_id),
-    FOREIGN KEY (media_id) REFERENCES media(id) ON DELETE CASCADE,
-    FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE CASCADE
-);
